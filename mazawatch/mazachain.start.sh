e97b37883363c24f7496b8b340c7261952a31e82
Rob Nelson <guruvan@users.noreply.github.com>
2014-12-28 10:45:03 -0800

    start to allow variables to be set from systemd/env files/docker env

--- mazawatch/mazachain.start.sh
+++ mazawatch/mazachain.start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+source /mazachain/env
+source /mazachain.env
+# allow variables to be set from systemd/env files/docker env 
+test -z $MAZACOIND && MAZACOIND=$(cat /mazachain/env |grep ${PROJECT^^}_DAEMON | grep ${RPCPORT}| grep TCP_ADDR | awk -F\= '{print $2}')
 
 MAZADIR=/home/maza/.mazacoin
 TESTNETDIR=${MAZADIR}/testnet3
@@ -14,7 +17,7 @@ if [ "${TESTNET}X" = "X" ] ; then
    NETWORK=livenet
    RPCPORT=$(grep rpcport ${MAZADIR}/mazacoin.conf |awk -F\= '{print $2}')
 fi
-MAZACOIND=$(cat /mazachain/env |grep ${PROJECT^^}_DAEMON | grep ${RPCPORT}| grep TCP_ADDR | awk -F\= '{print $2}')
+
 echo "$(date) starting mazawatch with RPC from: ${MAZACOIND}:${RPCPORT}"
 echo "BITCOIND_HOST= ${MAZACOIND} BITCOIN_DATADIR=${MAZADIR} BITCOIND_USER= ${RPCUSER} INSIGHT_NETWORK= ${NETWORK}"
 BITCOIND_HOST=${MAZACOIND} BITCOIN_DATADIR="${MAZADIR}" BITCOIND_USER=${RPCUSER} BITCOIND_PASS=${RPCPASSWORD}  INSIGHT_NETWORK=${NETWORK} npm start
