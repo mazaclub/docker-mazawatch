71f97f5587a0930c114747a48e614821d9863cee
Rob Nelson <guruvan@users.noreply.github.com>
2014-12-28 09:01:38 -0800

    fix missing )

--- mazawatch/mazachain.start.sh
+++ mazawatch/mazachain.start.sh
@@ -5,7 +5,7 @@ MAZADIR=/home/maza/.mazacoin
 TESTNETDIR=${MAZADIR}/testnet3
 NETWORK=testnet
 RPCPORT=11832
-RPCUSER=$(grep rpcuser ${MAZADIR}/mazacoin.conf |awk -F\= '{print $2}'
+RPCUSER=$(grep rpcuser ${MAZADIR}/mazacoin.conf |awk -F\= '{print $2}')
 RPCPASSWORD=$(grep rpcpassword ${MAZADIR}/mazacoin.conf |awk -F\= '{print $2}')
 TESTNET=$(grep testnet ${MAZADIR}/mazacoin.conf |awk -F\= '{print $2}')
 PROJECT=$(grep PROJECT /mazachain/env |tr '[a-z]' '[A-Z]'|awk -F\= '{print $2}')
@@ -14,8 +14,8 @@ if [ "${TESTNET}X" = "X" ] ; then
    NETWORK=livenet
    RPCPORT=$(grep rpcport ${MAZADIR}/mazacoin.conf |awk -F\= '{print $2}')
 fi
-MAZACOIND=`cat /mazachain/env |grep ${PROJECT^^}_DAEMON | grep ${RPCPORT}| grep TCP_ADDR | awk -F\= '{print $2}'`
+MAZACOIND=$(cat /mazachain/env |grep ${PROJECT^^}_DAEMON | grep ${RPCPORT}| grep TCP_ADDR | awk -F\= '{print $2}')
 echo "$(date) starting mazawatch with RPC from: ${MAZACOIND}:${RPCPORT}"
-echo "BITCOIND_HOST= ${MAZACOIND} BITCOIN_DATADIR=${MAZADIR} BITCOIND_USER= ${RPCUSER} BITCOIND_PASS= ${RPCPASSWORD}  INSIGHT_NETWORK= ${NETWORK} npm start"
+echo "BITCOIND_HOST= ${MAZACOIND} BITCOIN_DATADIR=${MAZADIR} BITCOIND_USER= ${RPCUSER} INSIGHT_NETWORK= ${NETWORK}"
 BITCOIND_HOST=${MAZACOIND} BITCOIN_DATADIR="${MAZADIR}" BITCOIND_USER=${RPCUSER} BITCOIND_PASS=${RPCPASSWORD}  INSIGHT_NETWORK=${NETWORK} npm start
 
